18a5d2319948013d9abdb4de9f97693df224a8d0
harniy <forbedlam@gmail.com>
2023-12-02 17:30:36 +0200

    fix: prodaction path

--- vite.config.ts
+++ vite.config.ts
@@ -7,33 +7,37 @@ import Components from 'unplugin-vue-components/vite'
 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    manifest: true,
-    chunkSizeWarningLimit: 1024,
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      imports: [
-        'vue',
-      ],
-      dts: 'types/auto-imports.d.ts',
-      dirs: ['src/hooks/**', 'src/stores/**'],
-    }),
-    Components({
-      // allow to autoload markdown components under `./src/components/`
-      extensions: ['vue', 'md', 'jsx', 'tsx'],
-      // allow auto import and register components used in markdown
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/, /\.jsx$/, /\.tsx$/],
-      resolvers: [],
-      dts: 'types/components.d.ts',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
-      '@/': `${path.resolve(__dirname, 'src')}/`,
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production'
+  return {
+    base: isProd ? '/telegram/' : '',
+    build: {
+      manifest: true,
+      chunkSizeWarningLimit: 1024,
     },
-  },
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: [
+          'vue',
+        ],
+        dts: 'types/auto-imports.d.ts',
+        dirs: ['src/hooks/**', 'src/stores/**'],
+      }),
+      Components({
+        // allow to autoload markdown components under `./src/components/`
+        extensions: ['vue', 'md', 'jsx', 'tsx'],
+        // allow auto import and register components used in markdown
+        include: [/\.vue$/, /\.vue\?vue/, /\.md$/, /\.jsx$/, /\.tsx$/],
+        resolvers: [],
+        dts: 'types/components.d.ts',
+      }),
+    ],
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
+        '@/': `${path.resolve(__dirname, 'src')}/`,
+      },
+    },
+  }
 })
